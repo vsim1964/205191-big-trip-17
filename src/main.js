3a24504a6df57840800be8001d158c5d9bdf233e
Siminenko <vsim1964@gmail.com>
2022-05-31 12:01:09 +0300

    Дополнительная правка

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import ListPresenter from './presenter/list-presenter.js';
 import PointsModel from './model/points-model.js';
 import RouteView from './view/route-view.js';
 import FilterModel from './model/filter-model.js';
-import NewPointButton from '../view/new-point-button-view';
+import NewPointButtonView from './view/new-point-button-view';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const tripMain = siteHeaderElement.querySelector('.trip-main');
 
 const siteMainElement = document.querySelector('.page-main');
 const listContainer = siteMainElement.querySelector('.page-body__container');
-
+const newPointButtonComponent = new NewPointButtonView();
 
 const pointModel = new PointsModel();
 const filterModel = new FilterModel();
@@ -19,6 +19,18 @@ const filterPresenter = new FilterPresenter(tripMain, filterModel, pointModel);
 const list = new ListPresenter(listContainer, pointModel, filterModel);
 
 render(new RouteView(), tripMain, 'afterbegin' );
+
+const handleNewPointFormClose = () => {
+  newPointButtonComponent.element.disabled = false;
+};
+
+const handleNewPointButtonClick = () => {
+  list.createTask(handleNewPointFormClose);
+  newPointButtonComponent.element.disabled = true;
+};
+
+render(newPointButtonComponent, tripMain);
+newPointButtonComponent.setClickHandler(handleNewPointButtonClick);
 
 filterPresenter.init();
 list.init();
--- src/view/new-point-button-view.js
+++ src/view/new-point-button-view.js
@@ -3,7 +3,7 @@ import AbstractView from '../framework/view/abstract-view';
 const createNewPointButtonTemplate = () =>
   '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
 
-export default class NewPointButton extends AbstractView  {
+export default class NewPointButtonView extends AbstractView  {
 
   get template() {
     return createNewPointButtonTemplate();
